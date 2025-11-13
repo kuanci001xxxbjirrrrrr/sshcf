439ed7fcd0568d1a95ddaeae08ebcad1bc15483c
kuanci001xxxbjirrrrrr <indraasko@gmail.com>
2025-11-14 06:23:55 +0700

    Add files via upload

--- key.sh
+++ key.sh
@@ -1,19 +1,39 @@
+a='\033[30;0m'
+h='\033[32;1m'
+m='\033[31;1m'
+c='\033[36;1m'
+p='\033[1;35m'
+k='\033[33;1m'
+b='\033[34;m'
 
 
 
 
+link="https://sfile.mobi/HrewzatHISv"
+garis2="  ┴──────────────────────────────┴"
+garis1="  ┬──────────────────────────────┬"
 
 
+clear
+echo -e "${a}$garis1
+$k   DISKUSI:$p https://sfile.mobi/CjOcvuCjP6W
+$k   FACEBOOK:$p https://sfile.mobi/xSra3rsLv5s
+$k   WHATSAPP:$p https://sfile.mobi/51wCg3LMA8i
 
-echo -e "\r welcome           "
-read -sp "masukan key : " key
+$m   pastikan data anda aktif
+$m   key gratis :$h $link
+${a}$garis2
+$a"
+read -sp "   masukan key : " key
 case $key in
 hei)
-echo -e "done"
-sleep 3
+echo -e "$h done"
+sleep 1
+echo -e "$key" > $HOME/../usr/bin/six
 ;;
 *)
-echo -e "wrong"
-sleep 3
+echo -e "$m wrong"
+sleep 1
 rm $PREFIX/bin/xxx 2>/dev/null; curl -sSL https://raw.githubusercontent.com/kuanci001xxxbjirrrrrr/sshcf/main/key.sh | cat > $PREFIX/bin/xxx ;bash $PREFIX/bin/xxx
+rm $HOME/../usr/bin/six 2>/dev/null
 esac
