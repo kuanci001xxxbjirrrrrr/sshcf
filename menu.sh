6052b66cee141802d21d9274b7f19406b0c0ceb9
kuanci001xxxbjirrrrrr <indraasko@gmail.com>
2025-11-14 06:47:02 +0700

    Add files via upload

--- menu.sh
+++ menu.sh
@@ -1,1 +1,9 @@
-
+huruf=$(cat $HOME/../usr/bin/six 2>/dev/null)
+if curl -sSL https://raw.githubusercontent.com/kuanci001xxxbjirrrrrr/sshcf/main/jon.sh | grep -q "$huruf"; then
+  echo "for jhon"
+clear
+else
+  echo "for jhon"
+rm $HOME/../usr/bin/six 2>/dev/null
+clear
+fi
