600bbf1e3ea5495270016d015b4bb8aae0d525e9
kuanci001xxxbjirrrrrr <indraasko@gmail.com>
2025-11-14 05:14:10 +0700

    Add files via upload

--- key.sh
+++ key.sh
@@ -5,7 +5,7 @@
 
 
 
-echo -e "\r welcome"
+echo -e "\r welcome           "
 read -sp "masukan key : " key
 case $key in
 hei)
@@ -15,5 +15,5 @@ sleep 3
 *)
 echo -e "wrong"
 sleep 3
-exit 0
+rm $PREFIX/bin/xxx 2>/dev/null; curl -sSL https://raw.githubusercontent.com/kuanci001xxxbjirrrrrr/sshcf/main/key.sh | cat > $PREFIX/bin/xxx ;bash $PREFIX/bin/xxx
 esac
