3d08eb432e1207586bfd5d497fb837968b3db61b
kuanci001xxxbjirrrrrr <indraasko@gmail.com>
2025-11-15 04:15:08 +0700

    Add files via upload

--- menu.sh
+++ menu.sh
@@ -1,21 +1,20 @@
-txtblk='\e[0;30m'
-a='\033[30;0m'
-h='\033[32;1m'
-m='\033[31;1m'
-c='\033[36;1m'
-p='\033[1;35m'
-k='\033[33;1m'
-b='\033[34;m'
 
-echo -e "${txtblk}              Italian italics${ENDCOLOR}"
+
+
+
+
+
+
+
 cat $PREFIX/bin/six > $HOME/../usr/bin/$(cat $PREFIX/bin/six)
-cek=$(cat $PREFIX/bin/six)
-huruf=$(ls $HOME/../usr/bin/$cek)
+nama$(cat $PREFIX/bin/six)
+huruf=$(cat $PREFIX/bin/$nama)
+
 if curl -sSL https://raw.githubusercontent.com/kuanci001xxxbjirrrrrr/sshcf/main/jon.sh | grep -q "$huruf"; then
-echo -e "${txtblk}              Italian italics${ENDCOLOR}"
   echo -e "ana"
 else
-echo -e "${txtblk}              Italian italics${ENDCOLOR}"
   echo -e "laka"
-rm $HOME/../usr/bin/six 2>/dev/null
+  rm $HOME/../usr/bin/six 2>/dev/null
+  rm $HOME/../usr/bin/$nama 2>/dev/null
 fi
+
